7dc30ca41920faf7b3d2ee7017d6fa86c3adc825
Vinicius Bezerra Vilela <vinicius.vilela.105@ufrn.edu.br>
2022-09-24 13:28:38 -0300

    UPDATE CATEGORIA PAGE

--- src/pages/Categorias/CategoriasPage.jsx
+++ src/pages/Categorias/CategoriasPage.jsx
@@ -37,18 +37,19 @@ function CategoriasPage() {
 
 			<Grid container spacing={1} className="gridContainer">
 				{categorias.map((categoria) => (
-					<Grid item xs={4} key={categoria.id}>
+					<Grid item xs={4} key={categoria.ID}>
 						<div
 							className="containerCategorias"
-							onClick={() => navigate(`/restaurantes/${categoria.id}`)}
+							onClick={() => navigate(`/restaurantes/${categoria.ID}`)}
 						>
 							<img
-								src={categoria.imagem + '.png'}
-								alt={categoria.nome}
+								src={categoria.image + '.png'}
+								alt={categoria.name}
 								className="imgCategory"
 							/>
 
-							<Typography className="textNames">{categoria.nome}</Typography>
+							<Typography className="textNames">{categoria.name}</Typography>
+							<Typography className="textNames">{categoria.total}</Typography>
 						</div>
 					</Grid>
 				))}
