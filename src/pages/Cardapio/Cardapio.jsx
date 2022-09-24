4c28c94e14834a0e72a8567777035312e0eb3c37
Lucas <prolucasloureiro@gmail.com>
2022-09-24 17:17:01 -0300

    feat: resposta da api

--- src/pages/Cardapio/Cardapio.jsx
+++ src/pages/Cardapio/Cardapio.jsx
@@ -14,51 +14,30 @@ import "./styles.css";
 function CardapioPage() {
 	const navigate = useNavigate();
     const [cardapio, setCardapio] = useState([])
+    const [nome, setNome] = useState()
+    const [descricao, setDescricao] = useState()
+    const [distancia, setDistancia] = useState()
 	const [loading, setLoading] = useState(true);
     const url = window.location.href;
     const idDetalhes = url.split("/")[4];
-    console.log(cardapio.categoria)
+    
 
 	useEffect(() => {
         getDetalhes(idDetalhes).then((response) => {
-          setCardapio(response.cardapio)
+          setCardapio(response)
+          setNome(response.nome)
+          setDescricao(response.descricao)
+          setDistancia(response.distancia)
           setLoading(false);
         })
       }, );
 
-	return (
-		<Container className="detalhes">
-			<Typography variant="h5" align="center" color="primary" className="title">
-				RESTAURANTES:
-            
-			</Typography>
-			{loading && (
-				<div className="loading">
-					<CircularProgress color="primary" />
-				</div>
-			)}
+      console.log(cardapio)
 
-			<Grid container spacing={1} className="gridContainer">
-				{cardapio.map((detalhes) => (
-					<Grid item xs={4} key={cardapio.ID}>
-						<div
-							className="containerCategorias"
-							onClick={() => navigate(`/restaurantes/${detalhes.ID}`)}
-						>
-							<img
-								src={cardapio.image + '.png'}
-								alt={detalhes.name}
-								className="imgCategory"
-							/>
-							<div className="name_total_card">
-								<Typography id="name" className="textNames">{cardapio.categoria}</Typography>
-								<Typography id="total" className="textNames">{detalhes.total}</Typography>
-							</div>
-						</div>
-					</Grid>
-				))}
-			</Grid>
-		</Container>
+	return (
+
+        <p>{cardapio.descricao} e {cardapio.distancia}</p>
+		
 	);
 }
 
